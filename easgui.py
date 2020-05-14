7fdd25eb1bcc95f5c7d4e854b3f668a8cee8e63f
joseamoroso <50180879+joseamoroso@users.noreply.github.com>
2020-05-14 17:20:04 -0500

    Edited

--- easgui.py
+++ easgui.py
@@ -2,33 +2,39 @@
 """
 Created on Thu May 14 01:06:33 2020
 
-@author: josel
+@author: Jose Amoroso
 """
 
 
 from textx import metamodel_from_file
-from tkinter import Tk,Button,Label,Entry
+from tkinter import Tk,Button,Label,Entry,Canvas,LEFT, RIGHT,HORIZONTAL,BOTH,TOP,BOTTOM,X
+from tkinter import ttk
 
+
+#REVISAR SINTAXIS DE WRAP, IGUAL QUE BORRE CONTENIDO ANTES DE CALCULAR UNO NUEVO        
+def wrap(func,args):
+    argsList = []
+    for value in args[:-1]:
+        argsList.append(int(value.get()))
+    argsTuple= tuple(argsList)
+    result = func(*argsTuple)
+    lastArg = args[-1]
+    lastArg.delete(0,"end")
+    lastArg.insert(0,str(result))
     
 class GUI:
     def __init__(self,master):
         self.functions = {}
         self.inputs = {}
         self.master = master
+        self.functionFlag = False
+        self.canvasPosition = (50,50)
         # Initial size of windows is (500,500) and name easgui
         master.title("EASGUI")
+        # self.canvas = Canvas(self.master, width=500,
+        #                     height=500, bg = "black")
         master.geometry("500x500")
 
-#REVISAR SINTAXIS DE WRAP, IGUAL QUE BORRE CONTENIDO ANTES DE CALCULAR UNO NUEVO        
-    def wrap(self,func,args):
-        argsList = []
-        for value in args[:-1]:
-            argsList.append(int(value.get()))
-        argsTuple= tuple(argsList)
-        result = func(*argsTuple)
-        lastArg = args[-1]
-        lastArg.delete(0,"end")
-        lastArg.insert(0,str(result))
 
     def interpret(self, model):
         for c in model.commands:
@@ -42,27 +48,39 @@ class GUI:
             if c.__class__.__name__ == "CreateWindow": 
                 self.master.title(c.NameID)
                 newSize = "{}x{}".format(c.coor1[0],c.coor1[1])
+                # self.canvas = Canvas(self.master, width=c.coor1[0],
+                #             height=c.coor1[1], bg = "black")
+                # self.canvas.pack()
                 self.master.geometry(newSize)
-                
-            if c.__class__.__name__ == "CreateInputs":
+
+
+            if c.__class__.__name__ == "CreateFunction":
+                #Insert line horizontal
+                if (self.functionFlag):
+                    self.separ = ttk.Separator(self.master, orient=HORIZONTAL)
+                    self.separ.pack(side=TOP,fill=BOTH, expand=True)
+                self.functionFlag = True
+                ######################################################################
+                ########################## Create inputs #############################
+                ######################################################################
                 for inputs in c.parameter:
-                    self.label = Label(self.master, text=inputs.inputName)
+                    self.label = Label(self.master, text=inputs.inputName+':', anchor='w')
                     self.entry = Entry(self.master)
                     self.inputs[inputs.inputName]=(self.label,self.entry)
-                    self.label.pack()
-                    self.entry.pack()
-            
-            if c.__class__.__name__ == "CreateButton":
+                    # self.canvas.create_window(self.canvasPosition[0],self.canvasPosition[1], window=self.entry)
+                    self.label.pack(side=TOP,  fill=X,padx=5, pady=5)
+                    self.entry.pack(side=TOP, expand=True)
+                ######################################################################
+                ################### Create button with function ######################
+                ######################################################################
                 tempListValues = []
-                #tempSignature = signature(self.functions[c.functionName])
                 for elem in self.inputs.values():
                     tempListValues.append(elem[1])
-                listValues = tuple(tempListValues)                    
-                # print(listValues)
-                funTemp = self.wrap
-                self.button = Button(self.master, text=c.buttonName, command=lambda:funTemp(self.functions[c.functionName],listValues))
-                self.button.pack()
-            
+                listValues = tuple(tempListValues)   
+                funTemp = wrap
+                self.button = Button(self.master, text=c.buttonName, command=lambda:funTemp(self.functions[c.functionName],listValues))      
+                self.button.pack(pady=5)
+                ######################################################################
 
             
 
