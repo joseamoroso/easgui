27a08634af9a9871da767f3a24096f8cfcd7d363
joseamoroso <50180879+joseamoroso@users.noreply.github.com>
2020-05-17 12:49:20 -0500

    Update easgui.py
    - Fixed multiple functions
    - Added more type of inputs
    - Added support for multiple results froms function (tuples, only)

--- easgui.py
+++ easgui.py
@@ -9,31 +9,75 @@ Created on Thu May 14 01:06:33 2020
 from textx import metamodel_from_file
 from tkinter import Tk,Button,Label,Entry,Canvas,LEFT, RIGHT,HORIZONTAL,BOTH,TOP,BOTTOM,X
 from tkinter import ttk
-
+from functools import partial
 
 #REVISAR SINTAXIS DE WRAP, IGUAL QUE BORRE CONTENIDO ANTES DE CALCULAR UNO NUEVO        
 def wrap(func,args):
+    # print("\n"+("#"*56))
+    # print(args)
     argsList = []
-    for value in args[:-1]:
-        argsList.append(int(value.get()))
-    argsTuple= tuple(argsList)
-    result = func(*argsTuple)
-    lastArg = args[-1]
-    lastArg.delete(0,"end")
-    lastArg.insert(0,str(result))
+    outlist = []
+    for value in args:
+        # print(value.get()+"\n")
+        if value[1] == 'INTINPUT':
+             argsList.append(int(value[0].get()))
+        if value[1] == 'DECIMALINPUT':
+             argsList.append(float(value[0].get()))
+             ###################################################
+        if value[1] == 'LISTINTINPUT':
+            tempList = value[0].get().split(',')
+            tempList = [int(i) for i in tempList]
+            argsList.append(tempList)
+        if value[1] == 'LISTIDECIMALINPUT':
+            tempList = value[0].get().split(',')
+            tempList = [float(i) for i in tempList]
+            argsList.append(tempList)
+        if value[1] == 'LISTSTRINGINPUT':
+            tempList = value[0].get().split(',')
+            tempList = [str(i) for i in tempList]
+            argsList.append(tempList)
+        if value[1] == 'STRINGINPUT':
+             argsList.append(str(value[0].get()))
+        if value[1] == 'OUTPUT':
+             outlist.append(value[0])
     
+    result = func(*tuple(argsList)) 
+    
+    if type(result)==tuple:
+        if len(outlist)<=1: 
+            print("INCOMPATIBLEN: not result is tuple")
+            return
+    
+    if len(outlist)>1:
+        if type(result)!=tuple:
+            print("INCOMPATIBLEN: not tuple result")
+            return
+        if len(result) != len(outlist) :
+            print("INCOMPATIBLEN between results and OUTPUTS")
+            return
+        else:
+            for i in range(len(outlist)):
+                outlist[i].delete(0,"end")
+                outlist[i].insert(0,str(result[i]))
+                
+    if len(outlist)==1:
+        outlist[0].delete(0,"end")
+        outlist[0].insert(0,str(result))
+        
 class GUI:
     def __init__(self,master):
+        self.co=0
         self.functions = {}
-        self.inputs = {}
         self.master = master
         self.functionFlag = False
-        self.canvasPosition = (50,50)
+        self.allButton ={}
+        # self.canvasPosition = (50,50)
         # Initial size of windows is (500,500) and name easgui
         master.title("EASGUI")
         # self.canvas = Canvas(self.master, width=500,
         #                     height=500, bg = "black")
         master.geometry("500x500")
+
 
 
     def interpret(self, model):
@@ -55,6 +99,9 @@ class GUI:
 
 
             if c.__class__.__name__ == "CreateFunction":
+                self.inputs = {}
+                self.listValues = None
+                self.button=None
                 #Insert line horizontal
                 if (self.functionFlag):
                     self.separ = ttk.Separator(self.master, orient=HORIZONTAL)
@@ -66,34 +113,35 @@ class GUI:
                 for inputs in c.parameter:
                     self.label = Label(self.master, text=inputs.inputName+':', anchor='w')
                     self.entry = Entry(self.master)
-                    self.inputs[inputs.inputName]=(self.label,self.entry)
+                    self.inputType = inputs.inputType
+                    self.inputs[inputs.inputName]=(self.label,(self.entry,self.inputType))
                     # self.canvas.create_window(self.canvasPosition[0],self.canvasPosition[1], window=self.entry)
                     self.label.pack(side=TOP,  fill=X,padx=5, pady=5)
                     self.entry.pack(side=TOP, expand=True)
                 ######################################################################
                 ################### Create button with function ######################
                 ######################################################################
-                tempListValues = []
+                self.tempListValues = []
                 for elem in self.inputs.values():
-                    tempListValues.append(elem[1])
-                listValues = tuple(tempListValues)   
-                funTemp = wrap
-                self.button = Button(self.master, text=c.buttonName, command=lambda:funTemp(self.functions[c.functionName],listValues))      
+                    self.tempListValues.append(elem[1])
+                self.listValues = tuple(self.tempListValues)
+                self.button = Button(self.master, text=c.buttonName)
+                # self.button.configure(command=lambda: wrap(self.functions[c.functionName],self.listValues))
+                self.button.configure(command=partial(wrap,self.functions[c.functionName],self.listValues))
                 self.button.pack(pady=5)
                 ######################################################################
+            
 
             
 
-def main(debug=False):
-    gui_mm = metamodel_from_file('easgui.tx')
-    
-    gui_model = gui_mm.model_from_file('program.eui')
-    
-    
-    root = Tk()
-    my_gui = GUI(root)
-    my_gui.interpret(gui_model)
-    root.mainloop()
 
-if __name__ == "__main__":
-    main()
+gui_mm = metamodel_from_file('easgui.tx')
+
+gui_model = gui_mm.model_from_file('program.eui')
+
+
+root = Tk()
+my_gui = GUI(root)
+my_gui.interpret(gui_model)
+
+root.mainloop()
