b02c58efa7d5a94938972b4684f17eed1139525b
joseamoroso <50180879+joseamoroso@users.noreply.github.com>
2020-05-27 13:37:01 -0500

    Added more error handling

--- easgui.py
+++ easgui.py
@@ -6,55 +6,24 @@ Created on Thu May 14 01:06:33 2020
 """
 
 
-from textx import metamodel_from_file,TextXSemanticError
+from textx import metamodel_from_file
+from errorMessages import TextXSemanticError, TextXRuntimeError
 from tkinter import Tk,Button,Label,Entry,LEFT,HORIZONTAL,BOTH,TOP,X,Frame,RAISED
 from tkinter import ttk
+from functionCaller import wrap, procMessage,bcolors,underlineStr
 from functools import partial
 import inspect
 import numpy as np
 import sys
 
-class bcolors:
-    HEADER = '\u001b[35;1m'
-    OKBLUE = '\u001b[34;1m'
-    OKGREEN = '\u001b[32;1m'
-    RED = '\033[91m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 
-color_flag = True    
-def procMessage(message, color , line, col, tupleInput):
-    if color_flag:
-        messageColor = "\n"+message +"\n\t" + color+tupleInput[1]+" "+tupleInput[0]+"\033[0m" + "\nIn:\n\t[line:"+str(line)+", col:"+str(col)+"]" 
-    else:
-        messageColor = "\n"+message +"\n\t" +tupleInput[1]+" "+tupleInput[0] + "\nIn:\n\t[line:"+str(line)+", col:"+str(col)+"]" 
-        
-    return messageColor
-    
+############    Semantics         ##############################   
 def checkSemantics(model, metamodel):
     modelCommands = model.commands
     inputIDList=[]
     for command in modelCommands:
         line,col = model._tx_parser.pos_to_linecol(command._tx_position)
-        # if command.__class__.__name__ == "ImportFunction":
-        #     try:
-        #         package = command.fileName
-        #         name = command.functionName
-        #     #se puede hacer un propio import primero del modeulo y despues de funciones para no usar importmodule de python
-        #     #semantico o runtime?
-        #         imported = getattr(__import__(package, fromlist=[name]), name)
-        #     # except ModuleNotFoundError:
-        #     #     raise ModuleNotFoundError("sss")
-        #     except ImportError:
-        #         print("no function")
-        #     except AttributeError as e:
-        #         raise ImportError(name + "does not exist in "+package)
-        #     except:
-        #         pass
-            
         if command.__class__.__name__ == "CreateFunction":
             #CHECK REPEATED IDS -> INPUTS
             for inputs in command.parameter:
@@ -62,80 +31,23 @@ def checkSemantics(model, metamodel):
                 inputType = inputs.inputType
                 if inputValue in inputIDList:
                     lineInput,colInput = model._tx_parser.pos_to_linecol(inputs._tx_position)
-                    messageError ="Repeated inputs IDs, check it is not used before."
-                    messageComp = inputValue,inputType
-                    message = procMessage(messageError,bcolors.RED,lineInput,colInput,messageComp)
+                    messageError ="Repeated inputs IDs, check it is not used before.\n\t{} {}".format(
+                                    underlineStr(inputValue),underlineStr(inputType))
+                    message = procMessage(messageError,bcolors.BLUE,lineInput,colInput)
                     raise TextXSemanticError(message,line=line,col=col,filename=model._tx_filename)
                 inputIDList.append((inputValue))
-        
 
-def wrap(func,args):
-    # print("\n"+("#"*56))
-    # print(args)
-    argsList = []
-    outlist = []
-    for value in args:
-        # print(value.get()+"\n")
-        if value[1] == 'INTINPUT':
-            try:
-                argsList.append(int(value[0].get()))
-            except:
-                raise RuntimeError("INTINPUT data type declared but another type was used")
-        if value[1] == 'MATRIXINPUT':
-            tempList=[]
-            tempMatrix = value[0].get().split(';')
-            for sublist in tempMatrix:
-                tempSplit = sublist.split(',')
-                tempSplit = [int(x) for x in tempSplit]
-                tempList.append(tempSplit)
-            tempList = np.array(tempList)
-            # print(tempList)
-            argsList.append(tempList)
-        if value[1] == 'DECIMALINPUT':
-             argsList.append(float(value[0].get()))
-        if value[1] == 'LISTINTINPUT':
-            tempList = value[0].get().split(',')
-            tempList = [int(i) for i in tempList]
-            argsList.append(tempList)
-        if value[1] == 'LISTIDECIMALINPUT':
-            tempList = value[0].get().split(',')
-            tempList = [float(i) for i in tempList]
-            argsList.append(tempList)
-        if value[1] == 'LISTSTRINGINPUT':
-            tempList = value[0].get().split(',')
-            tempList = [str(i) for i in tempList]
-            argsList.append(tempList)
-        if value[1] == 'STRINGINPUT':
-             argsList.append(str(value[0].get()))
-        if value[1] == 'OUTPUT':
-             outlist.append(value[0])
-    
-    result = func(*tuple(argsList))
-    # print(result)
-    
-    if type(result)==tuple:
-        if len(outlist)<=1: 
-            print("INCOMPATIBLEN: not result is tuple")
-            return
-    
-    if len(outlist)>1:
-        if type(result)!=tuple:
-            print("INCOMPATIBLEN: not tuple result")
-            return
-        if len(result) != len(outlist) :
-            print("INCOMPATIBLEN between results and OUTPUTS")
-            return
-        else:
-            for i in range(len(outlist)):
-                outlist[i].configure(state="normal")
-                outlist[i].delete(0,"end")
-                outlist[i].insert(0,str(result[i]))
-                
-    if len(outlist)==1:
-        outlist[0].configure(state="normal")
-        outlist[0].delete(0,"end")
-        outlist[0].insert(0,str(result))
+################################################################
 
+##########      Processors      ################################
+def createWindowProcessor(createWindowCmd):
+  # If steps is not given, set it do default 1 value.
+   if len(createWindowCmd.coor)==1:
+       createWindowCmd.coor.append(createWindowCmd.coor[0])
+   if len(createWindowCmd.coor)==0:
+       createWindowCmd.coor = [500,500]
+
+################################################################
         
 class GUI:
     def __init__(self,master):
@@ -143,47 +55,42 @@ class GUI:
         self.master = master
         self.functionFlag = False
         self.allButton ={}
-        # Initial size of windows is (500,500) and name easgui
-        master.title("EASGUI")
-
-        master.geometry("500x500")
-
-
 
     def interpret(self, model):
         for c in model.commands:
             if c.__class__.__name__ == "ImportFunction": 
                 package = c.fileName
                 name = c.functionName
-                imported = getattr(__import__(package, fromlist=[name]), name)
-                self.functions[c.newFunctionName]=imported
-                
-    
+                try:
+                    imported = getattr(__import__(package, fromlist=[name]), name)
+                    self.functions[c.newFunctionName]=imported
+                except ModuleNotFoundError:
+                    lineInput,colInput = model._tx_parser.pos_to_linecol(c._tx_position)
+                    messageError ="FILE {} NOT FOUND".format(underlineStr(package))
+                    message = procMessage(messageError,bcolors.BLUE,lineInput,colInput)
+                    raise TextXRuntimeError(message,line=lineInput,col=colInput,filename=model._tx_filename)
+                    sys.exit()
+                except AttributeError:
+                    lineInput,colInput = model._tx_parser.pos_to_linecol(c._tx_position)
+                    messageError ="FUNCTION {} NOT FOUND IN {}".format(underlineStr(name),underlineStr(package))
+                    message = procMessage(messageError,bcolors.BLUE,lineInput,colInput)
+                    raise TextXRuntimeError(message,line=lineInput,col=colInput,filename=model._tx_filename)
+                    sys.exit()
+
             if c.__class__.__name__ == "CreateWindow": 
-                try:
                     self.master.title(c.windowID)
-                except:
-                    pass
-                try:
                     newSize = "{}x{}".format(c.coor[0],c.coor[1])
                     self.master.geometry(newSize)
-                except:
-                    pass
-                try:
-                    newSize = "{}x{}".format(c.coor[0],c.coor[0])
-                    self.master.geometry(newSize)
-                except:
-                    pass
-
 
             if c.__class__.__name__ == "CreateFunction":
-                # raise TextXSyntaxError("Putito")
                 self.inputs = {}
                 self.listValues = None
                 self.button=None
                 #Create frame for each function
                 self.frame = Frame(self.master, relief=RAISED, borderwidth=1)
                 self.frame.pack(fill=BOTH, expand=True)
+                self.tempListValues = []
+
 
                 #Insert line horizontal
                 if (self.functionFlag):
@@ -196,27 +103,25 @@ class GUI:
                 for inputs in c.parameter:
                     self.frameInner = Frame(self.frame, relief=RAISED)
                     self.frameInner.pack(fill=BOTH, expand=True)
+                    line,col= model._tx_parser.pos_to_linecol(inputs._tx_position)
+                    self.inputPosition = (line,col)
+                    
                     self.inputType = inputs.inputType
-
                     if self.inputType == 'OUTPUT':
                         self.label = Label(self.frameInner, text=inputs.inputName+':', anchor='w',font=("Calibri", 10, "bold underline"))
                         self.entry = Entry(self.frameInner,state='disabled')
                     else:
                         self.label = Label(self.frameInner, text=inputs.inputName+':', anchor='w')
                         self.entry = Entry(self.frameInner)
-                        
-                    self.inputs[inputs.inputName]=(self.label,(self.entry,self.inputType))
+                    
+                    self.tempListValues.append((self.entry,self.inputType,self.inputPosition,inputs.inputName))
                     self.label.pack(side=LEFT,padx=5, pady=5)
                     self.entry.pack(side=LEFT,fill=X,expand=True,padx=10)
                 ######################################################################
                 ################### Create button with function ######################
                 ######################################################################
-                self.tempListValues = []
-                for elem in self.inputs.values():
-                    self.tempListValues.append(elem[1])
                 self.listValues = tuple(self.tempListValues)
                 self.button = Button(self.frame, text=c.buttonName)
-                # self.button.configure(command=lambda: wrap(self.functions[c.functionName],self.listValues))
                 self.button.configure(command=partial(wrap,self.functions[c.functionName],self.listValues))
                 self.button.pack(pady=5)
                 ######################################################################
@@ -225,14 +130,15 @@ class GUI:
 def main(debug=False):
 
     
-    gui_mm = metamodel_from_file('easgui.tx')
+    gui_mm = metamodel_from_file('tx_easgui/easgui.tx')
     
     gui_mm.register_model_processor(checkSemantics)
-    
+    gui_mm.register_obj_processors({'CreateWindow': createWindowProcessor})
+
     
     gui_model = gui_mm.model_from_file('program.eui')
     # gui_model = gui_mm.model_from_file(str(sys.argv[1]))
-    
+
     
     
     root = Tk()
---
+++ functionCaller.py
@@ -0,0 +1,146 @@
+from errorMessages import TextXSemanticError, TextXRuntimeError
+
+class bcolors:
+    HEADER = '\u001b[35;1m'
+    BLUE = '\u001b[34;1m'
+    OKGREEN = '\u001b[32;1m'
+    RED = '\033[91m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    
+def procMessage(message, color , line, col):
+    color_flag = True    
+    if color_flag:
+        messageColor = "\n"+message +"\n\t" + color + "\nIn:\n\t[line:"+str(line)+", col:"+str(col)+"]"+bcolors.ENDC 
+    else:
+        messageColor = "\n"+message +"\n\t" + "\nIn:\n\t[line:"+str(line)+", col:"+str(col)+"]" 
+        
+    return messageColor
+    
+def underlineStr(text):
+    return bcolors.UNDERLINE+text+bcolors.ENDC
+
+
+def wrap(func,args):
+    # print("\n"+("#"*56))
+    # print(args)
+    argsList = []
+    outlist = []
+    
+    for value in args:
+        # print(value.get()+"\n")
+        #ARREGLAR PARA VERIFICAR CAMPO VACIOS DE TODOS ANTES
+        if value[0].get()=='' and value[1] != 'OUTPUT':
+            lineInput,colInput = value[2]
+            messageError ="Empy data on entry defined at:\n\t{} {}".format(value[1],value[3])
+            message = procMessage(messageError,bcolors.BLUE,lineInput,colInput)
+            raise TextXRuntimeError(message,line=lineInput,col=colInput)
+        
+        if value[1] == 'INTINPUT':
+            try:
+                if "." in value[0].get():
+                    argsList.append(int(float(value[0].get())))
+                else:
+                    argsList.append(int(value[0].get()))
+            except ValueError:
+                lineInput,colInput = value[2]
+                messageError ="{} data type declared but another type was used:\n\t{} {}".format(value[1],value[1],value[3])
+                message = procMessage(messageError,bcolors.BLUE,lineInput,colInput)
+                raise TextXRuntimeError(message,line=lineInput,col=colInput)
+                
+        if value[1] == 'MATRIXINPUT':
+            try:
+                tempList=[]
+                tempMatrix = value[0].get().split(';')
+                for sublist in tempMatrix:
+                    tempSplit = sublist.split(',')
+                    tempSplit = [int(x) for x in tempSplit]
+                    tempList.append(tempSplit)
+                tempList = np.array(tempList)
+                argsList.append(tempList)
+            except:
+                lineInput,colInput = value[2]
+                messageError ="{} data type declared but another type was used:\n\t{} {}".format(value[1],value[1],value[3])
+                message = procMessage(messageError,bcolors.BLUE,lineInput,colInput)
+                raise TextXRuntimeError(message,line=lineInput,col=colInput)
+                
+        if value[1] == 'DECIMALINPUT':
+            try:
+                argsList.append(float(value[0].get()))
+            except ValueError:
+                lineInput,colInput = value[2]
+                messageError ="{} data type declared but another type was used: \n\t{} {}".format(value[1],value[1],value[3])
+                message = procMessage(messageError,bcolors.BLUE,lineInput,colInput)
+                raise TextXRuntimeError(message,line=lineInput,col=colInput)
+        
+        if value[1] == 'LISTINTINPUT':
+            try:
+                tempList = value[0].get().split(',')
+                tempList = [int(i) for i in tempList]
+                argsList.append(tempList)
+            except:
+                lineInput,colInput = value[2]
+                messageError ="{} data type declared but another type was used: \n\t{} {}".format(value[1],value[1],value[3])
+                message = procMessage(messageError,bcolors.BLUE,lineInput,colInput)
+                raise TextXRuntimeError(message,line=lineInput,col=colInput)
+                
+        if value[1] == 'LISTIDECIMALINPUT':
+            try:
+                tempList = value[0].get().split(',')
+                tempList = [float(i) for i in tempList]
+                argsList.append(tempList)
+            except:
+                lineInput,colInput = value[2]
+                messageError ="{} data type declared but another type was used: \n\t{} {}".format(value[1],value[1],value[3])
+                message = procMessage(messageError,bcolors.BLUE,lineInput,colInput)
+                raise TextXRuntimeError(message,line=lineInput,col=colInput)
+        if value[1] == 'LISTSTRINGINPUT':
+            try:
+                tempList = value[0].get().split(',')
+                tempList = [str(i) for i in tempList]
+                argsList.append(tempList)
+            except:
+                lineInput,colInput = value[2]
+                messageError ="{} data type declared but another type was used: \n\t{} {}".format(value[1],value[1],value[3])
+                message = procMessage(messageError,bcolors.BLUE,lineInput,colInput)
+                raise TextXRuntimeError(message,line=lineInput,col=colInput)
+        if value[1] == 'STRINGINPUT':
+            try:
+                argsList.append(str(value[0].get()))
+            except:
+                lineInput,colInput = value[2]
+                messageError ="{} data type declared but another type was used: \n\t{} {}".format(value[1],value[1],value[3])
+                message = procMessage(messageError,bcolors.BLUE,lineInput,colInput)
+                raise TextXRuntimeError(message,line=lineInput,col=colInput)
+                
+        if value[1] == 'OUTPUT':
+             outlist.append(value[0])
+             
+    #Declare function with arguments of input in entries
+    try:         
+        result = func(*tuple(argsList))
+    except:
+        raise TextXRuntimeError(message="INTERNAL ERROR OF FUNCTION")
+
+    
+    if len(outlist)>1:
+        if type(result)!=tuple:
+            raise TextXRuntimeError(message="INCOMPATIBLE: not tuple result of used function")
+        if len(result) != len(outlist) :
+            raise TextXRuntimeError(message="INCOMPATIBLE: different number of values at return and defined outputs")
+            
+        else:
+            for i in range(len(outlist)):
+                outlist[i].configure(state="normal")
+                outlist[i].delete(0,"end")
+                outlist[i].insert(0,str(result[i]))
+    
+    if len(outlist)==1:
+        if type(result)==tuple: 
+            raise TextXRuntimeError(message="INCOMPATIBLE: not tuple result of used function")
+        else:
+            outlist[0].configure(state="normal")
+            outlist[0].delete(0,"end")
+            outlist[0].insert(0,str(result))
